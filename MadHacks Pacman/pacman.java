810f5b628f8866603e28adc7a6d94f26f564e172
jdoenier <jdoenier@wisc.edu>
2017-02-18 13:54:16 -0600

    Update pacman.java

--- MadHacks Pacman/pacman.java
+++ MadHacks Pacman/pacman.java
@@ -1,5 +1,37 @@
 
-public class pacman {
-hi
-this is a commit from eclipse	
+package com.zetcode;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class pacman extends JFrame {
+    
+    public Application() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setSize(250, 200);
+
+        setTitle("Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }    
+    
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Application ex = new Application();
+                ex.setVisible(true);
+            }
+        });
+    }
 }
+

055247cd6332b4a4c31971abacdafab4548f90b5
j2day2 <jeday@wisc.edu>
2017-02-19 02:15:34 -0600

    Add files via upload

---
+++ Hackathon/Pacman.java
@@ -0,0 +1,139 @@
+
+import java.awt.EventQueue;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Pacman extends JFrame {
+	private int dx;
+	private int dy;
+	private int x;
+	private int y;
+	int speed;
+	private int[] _dx;
+	private int[] _dy;
+	private Image image;
+	private int viewDX;
+	private int viewDY;
+
+	public Pacman(Board board, int x, int y, int dx, int dy, int speed, int viewDX, int viewDY) {
+		image = new ImageIcon("Images/pacman.png").getImage();
+		this.x = x;
+		this.y = y;
+		this.dx = dx;
+		this.dy = dy;
+		this.speed =speed;
+		this.viewDX = viewDX;
+		this.viewDY = viewDY;
+		_dx = new int[4];
+		_dy = new int[4];
+	}
+
+	public void move(Board board) {
+		int blockSize = board.getBlockSize();
+		int numBlocks = board.getNumBlocks();
+
+		int pos;
+		int count;
+		short ch;
+		
+		
+
+		if (x % blockSize == 0 && y % blockSize == 0) {
+			pos = x / blockSize + numBlocks * (int) (y / blockSize);
+			ch = board.screenData[pos];
+			
+			if ((ch & 16) != 0) {
+			board.screenData[pos] = (short) (ch & 15);
+			board.decrementScore();
+			}
+			count = 0;
+
+			if ((board.screenData[pos] & 1) == 0 && dx != 1) {
+				_dx[count] = -1;
+				_dy[count] = 0;
+				count++;
+			}
+
+			if ((board.screenData[pos] & 2) == 0 && dy != 1) {
+				_dx[count] = 0;
+				_dy[count] = -1;
+				count++;
+			}
+
+			if ((board.screenData[pos] & 4) == 0 && dx != -1) {
+				_dx[count] = 1;
+				_dy[count] = 0;
+				count++;
+			}
+
+			if ((board.screenData[pos] & 8) == 0 && dy != -1) {
+				_dx[count] = 0;
+				_dy[count] = 1;
+				count++;
+			}
+
+			if (count == 0) {
+
+				if ((board.screenData[pos] & 15) == 15) {
+					dx = 0;
+					dy = 0;
+				} else {
+					this.dx = -dx;
+					dy = -dy;
+				}
+
+			} else {
+
+				count = (int) (Math.random() * count);
+
+				if (count > 3) {
+					count = 3;
+				}
+
+				dx = _dx[count];
+				dy = _dy[count];
+				viewDX= dx;
+				viewDY = dy;
+			}
+		}
+		x = x + (dx * speed);
+		y = y + (dy * speed);
+		//board.drawPacman(board.g2d, this);
+
+		// if (pacman_x > (ghost_x[i] - 12) && pacman_x < (ghost_x[i] + 12)
+		// && pacman_y > (ghost_y[i] - 12) && pacman_y < (ghost_y[i] + 12)
+		// && inGame) {
+		//
+		// dying = true;
+		// }
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+	
+	public int getViewDX(){
+		return viewDX;
+	}
+	
+	public int getViewDY(){
+		return viewDY;
+	}
+
+	// public void drawPacman() {
+	// //fill this in
+	// }
+
+}
---
+++ Hackathon/Play.java
@@ -0,0 +1,30 @@
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Play extends JFrame {
+
+    public Play() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+        
+        add(new Board());
+        
+        setTitle("Pacman");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(380, 450);
+        setLocationRelativeTo(null);
+        setVisible(true);        
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            Play ex = new Play();
+            ex.setVisible(true);
+        });
+    }
+}
